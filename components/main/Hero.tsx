8680531f0b5fcf0fe5640b80eafe518346413d84
Reza Karim <reza.karimkeystone@gmail.com>
2023-12-26 11:33:37 +0600

    Status: Working

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
         loop
         className='rotate-180 absolute top-[-340px] left-0 z-[1] w-full h-full object-cover'
         >
-            <source src='blackhole.webm' type='video/webm' />
+            <source src='/blackhole.webm' type='video/webm' />
         </video>
     </div>
   )
